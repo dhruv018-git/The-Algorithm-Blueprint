d3a1bead235b7bbf910e7939543de00015d13152
dhruv jain <dhruvj878@gmail.com>
2026-03-15 21:22:55 +0530

    Time: 58 ms (6.08%), Space: 53.3 MB (92.48%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -6,11 +6,12 @@
  */
 var reduce = function(nums, fn, init) {
     let val = init;
-
+    
+    // Array ke har element par loop chalayenge
     for (let i = 0; i < nums.length; i++) {
+        // Purani 'val' aur current element ko function mein pass karke 'val' update karenge
         val = fn(val, nums[i]);
     }
-
+    
     return val;
 };
-
