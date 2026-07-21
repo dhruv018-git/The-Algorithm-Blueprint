4683840e7314a95db2904f4c4e6c5d662579d122
dhruv jain <dhruvj878@gmail.com>
2026-07-21 23:59:43 +0530

    Time: 0 ms (100%), Space: 60.6 MB (44.04%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+   let i = 0;
+   let n = nums.length;
+   for(let j=0;j<n;j++){
+    if(nums[j]!==0){
+        
+     let temp = nums[j];
+        nums[j]=nums[i];
+        nums[i]=temp;
+        i++
+    }
+   }
+
+    
+};
