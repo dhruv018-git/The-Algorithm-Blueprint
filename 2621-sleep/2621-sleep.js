ee28d8e13f2974fe53de0a40204410e4ec20fd89
dhruv jain <dhruvj878@gmail.com>
2026-08-08 09:20:44 +0530

    Time: 53 ms (12.69%), Space: 52.2 MB (97%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -3,5 +3,13 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-  return new Promise(resolve => setTimeout(resolve, millis));
+    return new Promise((resolve)=>{
+        setTimeout(resolve,millis)
+    }
+    );
 }
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
